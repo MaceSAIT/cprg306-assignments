7ae473aa68436a97598547fbfcb3d0310ea270cc
MaceSAIT <166629082+MaceSAIT@users.noreply.github.com>
2025-02-04 09:56:07 -0700

    small changes to main page

--- app/page.js
+++ app/page.js
@@ -1,21 +1,43 @@
-
+//import { StyleSheet, Text, View } from 'react-native';
 import Link from 'next/link'
 
 export default function Home() {
     return (
       <main>
         <div>
-            <h1>Mace's Week 2 App</h1>
+            <h1 className="text-3xl font-bold m-6 text-center">Mace's CPRG 306 Assignments</h1>
             <h2>Home</h2>
             <br></br>
             <br></br>
-            <Link href={{pathname:'/week-2'}}>Week 2 Page</Link>
-            <br></br>
-            <Link href={{pathname:'/week-3'}}>Week 3 Page</Link>
-            <br></br>
-            <Link href={{pathname:'/week-4'}}>Week 4 Page</Link>
+
+            <ul>
+              <li>
+                <Link href={{pathname:'/week-2'}}>Week 2 Page</Link>
+              </li>
+              <li>
+              <Link href={{pathname:'/week-3'}}>Week 3 Page</Link>
+              </li>
+              <li>
+              <Link href={{pathname:'/week-4'}}>Week 4 Page</Link>
+              </li>
+              <li>
+              <Link href={{pathname:'/week-5'}}>Week 5 Page</Link>
+              </li>
+            </ul>
         </div>
         
       </main>
     );
   }
+
+/*{
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: 'column',
+      backgroundColor: '#F3F2F8',
+      //alignItems: 'center',
+      justifyContent: 'flex-start',
+    },
+});
+}*/
